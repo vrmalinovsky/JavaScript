1881380dede617583b40c46d56444096e5d7da47
vrmalinovsky <vrmalinovsky@mail.ru>
2016-04-15 16:22:51 +0300

    Task5 - Bean Counting

---
+++ Task5/test.js
@@ -0,0 +1,18 @@
+describe("countBs()", function() {
+
+  it("countBs('BBC') should return 2", function() {
+    assert.equal(countBs("BBC"), 2);
+  });
+
+
+});
+
+describe("countChar()", function() {
+
+ it("countChar('kakkerlak', 'k') should return 4 ", function() {
+    assert.equal(countChar("kakkerlak", "k"), 4);
+  });
+
+});
+
+ 
