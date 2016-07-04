da714952b44199177a3944aecf315d675a045393
vrmalinovsky <vrmalinovsky@mail.ru>
2016-07-04 15:53:16 +0300

    Task 12 - Mother and child age difference
    Mother and child age difference

---
+++ Task 12/test.js
@@ -0,0 +1,10 @@
+describe("difference", function() {
+
+  it("average(differences) should return 31.22222222222222 ", function() {
+  	assert.deepEqual(average(differences), 31.22222222222222);
+  });
+
+});
+
+
+ 
