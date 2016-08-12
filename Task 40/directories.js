b7aea0e0f5b2a3b3de5479ffca6d1728307cd90b
vrmalinovsky <vrmalinovsky@mail.ru>
2016-08-12 09:43:00 +0300

    Task 40 - Creating directories
    Creating directories

---
+++ Task 40/directories.js
@@ -0,0 +1,13 @@
+methods.MKCOL = function(path, respond) {
+  fs.stat(path, function(error, stats) {
+    if (error && error.code == "ENOENT") {
+      fs.mkdir(path, respondErrorOrNothing(respond));
+    } else if (error) {
+      response(500, error.toString());
+    } else if (stats.isDirectory()) {
+      respond(204);
+    } else {
+      respond(400, "File exists");
+    }
+  });
+};
